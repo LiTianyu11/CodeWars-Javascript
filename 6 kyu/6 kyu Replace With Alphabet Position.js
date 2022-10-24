ed1dbb77a44ebca4f81bf95ba56e5ce2067f9168
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-24 16:51:10 +0800

    change

--- 6 kyu/6 kyu Replace With Alphabet Position.js
+++ 6 kyu/6 kyu Replace With Alphabet Position.js
@@ -17,3 +17,37 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 #STRINGS  #FUNDAMENTALS
 */
+
+//1️⃣ Solution 1
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    var code = text.toUpperCase().charCodeAt(i);
+    if (code > 64 && code < 91) result += code - 64 + " ";
+  }
+
+  return result.slice(0, result.length - 1);
+}
+
+//2️⃣ Solution2
+let alphabetPosition = (text) =>
+  text
+    .toUpperCase()
+    .replace(/[^A-Z]/g, "")
+    .split("")
+    .map((ch) => ch.charCodeAt(0) - 64)
+    .join(" ");
+
+//3️⃣ Solution 3
+function alphabetPosition(text) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((letter) => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    })
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(" ");
+}
