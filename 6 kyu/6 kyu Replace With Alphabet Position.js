8f31d994e5136e651d0e969e308969b415f085cd
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-24 16:39:23 +0800

    c

--- 6 kyu/6 kyu Replace With Alphabet Position.js
+++ 6 kyu/6 kyu Replace With Alphabet Position.js
@@ -1,4 +1,8 @@
 /*
+🔸Replace With Alphabet Position
+https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+DESCRIPTION:
 Welcome.
 
 In this kata you are required to, given a string, replace every letter with its position in the alphabet.
@@ -11,4 +15,5 @@ Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
+#STRINGS  #FUNDAMENTALS
 */
